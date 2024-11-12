6ddd386134c92b2bf732a18ccc8a4ed21e56f08b
krish kumar singh <krishkumarsingh9939@gmail.com>
2024-11-12 13:19:32 +0530

    add new features in todo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import AddTodo from "./components/AddTodo"
+import Todo from "./components/Todo"
+import "./App.css"
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <AddTodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
